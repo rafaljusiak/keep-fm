cf8b01e7bd52e5375f378107cd04036f79d07dee
Rafal Jusiak <jusiak.raf@gmail.com>
2020-05-13 19:52:31 +0200

    project setup

---
+++ keep_fm/settings.py
@@ -0,0 +1,122 @@
+import os
+import env_loader
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+env = env_loader.load_env(ROOT_DIR, env_type=env_loader.EnvTypes.DEVELOPMENT_ENV)
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env.get("SECRET_KEY", None)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.get("DEBUG", False)
+
+ALLOWED_HOSTS = env.get("ALLOWED_HOSTS", [])
+
+
+# Application definition
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+PROJECT_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "keep_fm.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "keep_fm.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.get("DATABASE_NAME", "postgres"),
+        "USER": env.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": env.get("POSTGRES_PASSWORD", ""),
+        "HOST": env.get("POSTGRES_HOST", "db"),
+        "PORT": env.get("POSTGRES_PORT", 5432),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = "/static/"
+
+
+# Celery
+
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_BEAT_SCHEDULE = {
+    # ...
+}
---
+++ tasks.py
@@ -0,0 +1,74 @@
+from invoke import task
+
+DOCKER = "docker-compose"
+DOCKER_RUN = f"{DOCKER} run --rm django"
+
+
+def docker(c, command, **kwargs):
+    c.run(f"{DOCKER} {command}", **kwargs)
+
+
+def docker_run(c, command, **kwargs):
+    c.run(f"{DOCKER_RUN} {command}", **kwargs)
+
+
+@task
+def build(c):
+    docker(c, "build")
+
+
+@task
+def start(c):
+    docker(c, "up -d")
+
+
+@task
+def stop(c):
+    docker(c, "stop")
+
+
+@task
+def makemigrations(c, args=""):
+    docker_run(c, f"python manage.py makemigrations {args}")
+
+
+@task
+def migrate(c):
+    docker_run(c, "python manage.py migrate")
+
+
+@task
+def collectstatic(c):
+    docker_run(c, "python manage.py collectstatic --noinput")
+
+
+@task
+def quality(c):
+    docker_run(c, "black .")
+    docker_run(c, "flake8 .")
+
+
+@task
+def quality_check(c):
+    docker_run(c, "black . --check")
+    docker_run(c, "flake8 .")
+
+
+@task
+def bandit(c):
+    docker_run(c, "bandit -r .")
+
+
+@task
+def add_dep(c, dependency):
+    docker_run(c, "poetry add " + dependency)
+
+
+@task
+def add_dev_dep(c, dependency):
+    docker_run(c, "poetry add -D " + dependency)
+
+
+@task
+def update_dep(c, dependencies):
+    docker_run(c, "poetry update " + dependencies)
