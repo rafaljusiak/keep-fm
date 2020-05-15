a6d5c5000761bda64c0ad4ca82a0e5a533d0a6f3
Rafal Jusiak <jusiak.raf@gmail.com>
2020-05-16 00:02:56 +0200

    fixed user admin

--- keep_fm/users/admin.py
+++ keep_fm/users/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from keep_fm.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = (
         "id",
         "username",
         "email",
         "lastfm_username",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Configuration", {"fields": ("lastfm_username",)}),
     )
