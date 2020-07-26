135f7f0ccb53b96ad89d63f74eec9b2042d97642
Rafal Jusiak <jusiak.raf@gmail.com>
2020-07-26 11:15:04 +0200

    allow unicode in slugs

--- keep_fm/tracks/slugs.py
+++ keep_fm/tracks/slugs.py
@@ -3,4 +3,4 @@ from django.utils.text import slugify
 
 def slugify_track(artist_name, track_name):
     full_name = f"{artist_name} {track_name}"
-    return slugify(full_name)
+    return slugify(full_name, allow_unicode=True)
