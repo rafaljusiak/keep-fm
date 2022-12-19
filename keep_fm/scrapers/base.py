6a265291754cb6a9cfe357f19f19b86e1e14cf10
Rafal Jusiak <jusiak.raf@gmail.com>
2022-12-19 10:57:15 +0100

    Add user-agent

--- keep_fm/scrapers/base.py
+++ keep_fm/scrapers/base.py
@@ -1,5 +1,6 @@
 import abc
 import time
+import requests
 from typing import Tuple, Any, Optional
 
 import urllib3
@@ -110,7 +111,7 @@ class Scraper(abc.ABC):
         page content.
         """
         r = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
-        soup = BeautifulSoup(r.data, "html.parser")
+        soup = BeautifulSoup(r.text, "html.parser")
         return soup
 
     def pre_run(self) -> None:
